0ae61684a4487161f851bda78ef5372f3d95b35a
Jorge Dias <jorgediasdsg@gmail.com>
2020-09-19 10:38:08 -0300

    🚀 Chore: state and css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header'
 
+import './App.css'
+import backgroundImage from './assets/background.jpeg';
+/**
+ * Component
+ * Props
+ * State
+ */
+
 function App() {
+    
+    const [projects, setProjects] = useState(['Dev', 'Devops']);
+
+    function handleAddProject(){
+        setProjects([...projects, `New project ${Date.now()}`]);
+    }
+
     return (
         <>
-            <Header />
-            <Header />
-            <Header />
+            <Header title="Projects" />
+
+            <img width="300" src={backgroundImage} />
+
+            <button type="buttom" onClick={handleAddProject}>Adicionar</button>
+            <ul>
+                {projects.map(project => <li key={project}>{project}</li>)}
+            </ul>
         </>
     )
 }
